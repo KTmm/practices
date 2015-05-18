c30dc68b3bf2bf553a09d1e5dc879a90f0e084a0
Ying Zhou <cherubzhou@gmail.com>
2015-05-18 13:37:52 -0400

    modify test cases

--- FindLongUpRunTest.java
+++ FindLongUpRunTest.java
@@ -24,26 +24,30 @@ public class FindLongUpRunTest {
     @Test
     public void test1(){
     	StockPrices sp1 = new StockPrices(prices1);
-    	sp1.findLongestUpRun();
-    	assertEquals(0, sp1.getLongestUpRun());
+    	assertEquals(0, sp1.findLongestUpRunPeriod());
     	assertEquals(0, sp1.getPriceGain(), 0.001);
     }
     
     @Test
     public void test2(){
     	StockPrices sp2 = new StockPrices(prices2);
-    	sp2.findLongestUpRun();
-    	assertEquals(1, sp2.getLongestUpRun());
+    	assertEquals(1, sp2.findLongestUpRunPeriod());
     	assertEquals(0, sp2.getPriceGain(), 0.001);
     }
     
     @Test
     public void test3(){
     	StockPrices sp3 = new StockPrices(prices3);
-    	sp3.findLongestUpRun();
-    	assertEquals(2, sp3.getLongestUpRun());
+    	assertEquals(2, sp3.findLongestUpRunPeriod());
+    	assertEquals(0.0, sp3.getPriceGain(), 0.001);
     }
 
+    @Test
+    public void test4(){
+    	StockPrices sp4 = new StockPrices(prices4);
+    	assertEquals(3, sp4.findLongestUpRunPeriod());
+    	assertEquals(2.00, sp4.getPriceGain(), 0.001);
+    }
 	
 
 }
